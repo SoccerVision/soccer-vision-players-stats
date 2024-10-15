82292d158bb896743d9abe54d904a35cd491bca1
Idan Levian <eidan47@gmail.com>
2024-10-15 15:48:33 +0300

    Fix CORS errors

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/idanlevian/Desktop/projects/soccerSimulator/venv"
+set -gx VIRTUAL_ENV "/Users/idanlevian/Desktop/projects/soccerSimulatorOrganization/Soccer-Simulation-BE/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
